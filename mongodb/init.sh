ebf0188d930b45967d169becefcc0dc634e7e4c1
Ruchan Yalçın <ruchany13@gmail.com>
2022-04-20 16:46:52 +0300

    add description
    init script created

---
+++ mongodb/init.sh
@@ -0,0 +1,9 @@
+use stajdb
+
+db.createCollection("ulke,il")
+
+db.il.insert({"Yalova", "İstanbul","Konya","Karaman","Ankara"
+"Bursa","İzmir","Düzce","Bolu","Hatay"})
+
+db.ulke.insert({"ABD","Türkiye","Fransa","Rusya","Gürcistan",
+"Bulgaristan","İtalya","Almanya","Çekya","Hollanda"})
