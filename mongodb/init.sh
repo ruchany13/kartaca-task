d5e487374ba6c9de2722a7572d2eca2dddae9358
Ruchan Yalçın <ruchany13@gmail.com>
2022-04-22 01:12:56 +0300

    adding mongodb replica set and an example python app

--- mongodb/init.sh
+++
@@ -1,9 +0,0 @@
-use stajdb
-
-db.createCollection("ulke,il")
-
-db.il.insert({"Yalova", "İstanbul","Konya","Karaman","Ankara"
-"Bursa","İzmir","Düzce","Bolu","Hatay"})
-
-db.ulke.insert({"ABD","Türkiye","Fransa","Rusya","Gürcistan",
-"Bulgaristan","İtalya","Almanya","Çekya","Hollanda"})
